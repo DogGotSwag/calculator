6bb46961db405cf13320902f26c08d5e7fc38ddd
Didier <115608922+DogGotSwag@users.noreply.github.com>
2023-12-07 22:50:30 -0700

    create display function
    minor cosmetic tweaks changeed enter to =
    and changed buttons slightly so the equals is the same size as teh rest

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ function operate( a, op , b){
 let numOne = undefined;
 let currOp = undefined;
 let numTwo = undefined;
+
+let display = document.querySelector(".display");
+
+function setDisplay( string ){
+    display.textContent = string;
+}
+
+setDisplay("Hello");
